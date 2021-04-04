12a61e529cb191de163c42eee9999d15cad5ca54
Jarvis Chou <wx_32298279f933433db9770f4f0c8a7398@git.code.tencent.com>
2021-04-05 00:55:10 +0800

    第九周作业提交

--- Week_09/RelativeSortArray.java
+++ Week_09/RelativeSortArray.java
@@ -1,21 +1,24 @@
-public class RelativeSortArray{
-	public int[] relativeSortArray(int[] arr1, int[] arr2) {
-   	 int[] res=new int[arr1.length];
-   	 int index=0;
-   	 int[] times=new int [1001];  //因为数范围在0-1000
-   	 for(int num:arr1)times[num]++;  //遍历arr1
-   		 for(int num:arr2){          //遍历arr2
-       	 while(times[num]>0){
-            res[index++]=num;
-            times[num]--;
-      	  }
-    	}
-    	for(int i=0;i<times.length;i++){
-        while(times[i]>0){
-            res[index++]=i;
-            times[i]--;
+/**
+ * 题目：数组的相对排序
+ */
+public class RelativeSortArray {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] res = new int[arr1.length];
+        int index = 0;
+        int[] times = new int[1001];  //因为数范围在0-1000
+        for (int num : arr1) times[num]++;  //遍历arr1
+        for (int num : arr2) {          //遍历arr2
+            while (times[num] > 0) {
+                res[index++] = num;
+                times[num]--;
+            }
         }
-   	  }
-      return res;
-  }
+        for (int i = 0; i < times.length; i++) {
+            while (times[i] > 0) {
+                res[index++] = i;
+                times[i]--;
+            }
+        }
+        return res;
+    }
 }
